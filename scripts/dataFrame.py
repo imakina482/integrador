9769c68f8cb243d62e9baa73f7b602ca3b194542
imakina <imakina482@gmail.com>
2024-10-14 23:53:28 -0300

    primer tests

--- scripts/dataFrame.py
+++ scripts/dataFrame.py
@@ -1,26 +1,28 @@
 import requests
 import pandas as pd
 
-# URL de la API de Binance para obtener el precio del par de criptomonedas
-url = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=100'
+def transform_data():
+    # URL de la API de Binance para obtener el precio del par de criptomonedas
+    url = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=100'
 
-# GET
-response = requests.get(url)
+    # GET
+    response = requests.get(url)
 
-if response.status_code == 200:  
-    data = response.json()
-    
-    # Creo un DF con los datos
-    df = pd.DataFrame(data, columns=['Open Time', 'Open', 'High', 'Low', 'Close', 'Volume', 
-                                      'Close Time', 'Quote Asset Volume', 'Number of Trades', 
-                                      'Taker Buy Base Asset Volume', 'Taker Buy Quote Asset Volume', 
-                                      'Ignore'])
+    if response.status_code == 200:  
+        data = response.json()
 
-    # Convierto la columna 'Open Time' y 'Close Time' a formato de fecha
-    df['Open Time'] = pd.to_datetime(df['Open Time'], unit='ms')
-    df['Close Time'] = pd.to_datetime(df['Close Time'], unit='ms')
+        # Creo un DF con los datos
+        df = pd.DataFrame(data, columns=['Open Time', 'Open', 'High', 'Low', 'Close', 'Volume', 
+                                         'Close Time', 'Quote Asset Volume', 'Number of Trades', 
+                                         'Taker Buy Base Asset Volume', 'Taker Buy Quote Asset Volume', 
+                                         'Ignore'])
 
-    # Muestro las primeras filas del DataFrame
-    print(df.head())
-else:
-    print("Error en la solicitud:", response.status_code)
+        # Convierto la columna 'Open Time' y 'Close Time' a formato de fecha
+        df['Open Time'] = pd.to_datetime(df['Open Time'], unit='ms')
+        df['Close Time'] = pd.to_datetime(df['Close Time'], unit='ms')
+
+        # Retorno el DataFrame
+        return df
+    else:
+        print("Error en la solicitud:", response.status_code)
+        return None
---
+++ tests/test_binance_data.py
@@ -0,0 +1,14 @@
+import unittest
+from scripts.dataFrame import transform_data
+import pandas as pd
+
+class TestBinanceData(unittest.TestCase):
+    
+    def test_transform_data(self):
+        df = transform_data()
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertEqual(len(df.columns), 12) 
+        self.assertIn('Open Time', df.columns)
+
+if __name__ == '__main__':
+    unittest.main()
